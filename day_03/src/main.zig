75a2688c40c7bc96a12e9fa7096a036f9536e9bd
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-06-26 23:33:32 -0300

    Finishes the ArrayList function

--- day_03/src/main.zig
+++ day_03/src/main.zig
@@ -14,19 +14,13 @@ pub fn main() !void {
     var buf: [1024]u8 = undefined;
 
     while (try buf_reader.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const value = ArrayList(u8).init(allocator);
-        const origin = std.mem.trim(u8, line, "\r");
-        try value.append(origin[0..origin.len]);
-        // for (origin) |originChar| {
-        //     std.log.info("{}", .{@typeInfo(@TypeOf(originChar))});
-        //     value.append(originChar);
-        // }
-        // value.append(origin[0..origin.len]);
+        var value = ArrayList(u8).init(allocator);
+        try value.appendSlice(std.mem.trim(u8, line, "\r"));
         try list.append(value);
     }
 
     for (list.items) |itemsList| {
-        std.log.info("3 {any}", .{itemsList.items});
+        std.log.info("{any}", .{itemsList.items});
     }
 
     // std.log.info("3 {any}", .{list.items});
