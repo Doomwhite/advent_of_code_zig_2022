7c92f9b0c210c8b7ea65aacb31dd0264e89874d9
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-07-16 19:42:06 -0300

    Finishes the part 02 from day 06

--- day_06/part_02/src/main.zig
+++ day_06/part_02/src/main.zig
@@ -2,45 +2,41 @@ const std = @import("std");
 const utils = @import("utils");
 const BoundedArray = std.BoundedArray;
 
-const min_char_count_for_marker: comptime_int = 3;
+const min_char_count_for_marker: comptime_int = 14;
 
 pub fn main() !void {
-    var list = try BoundedArray(u8, 4).init(4);
+    var list = try BoundedArray(u8, min_char_count_for_marker).init(min_char_count_for_marker);
 
     const string: []const u8 = utils.getStringFromPath("src/input.txt") catch "";
 
     const index = for_loop: for (string, 0..) |character, char_index| {
-        if (list.len != 4) {
+        std.log.info("{any}", .{char_index});
+        if (list.len != min_char_count_for_marker) {
             try list.append(character);
         } else {
             const replaced_array: []const u8 = &list.buffer[1..].* ++ [1]u8{character};
-            try list.replaceRange(0, 4, replaced_array);
+            try list.replaceRange(0, min_char_count_for_marker, replaced_array);
         }
+        std.log.info("{s}", .{list.buffer});
         if (char_index > min_char_count_for_marker) {
             var is_different_count: u8 = 0;
             for (list.buffer, 0..) |list_item, item_index| {
-                if (item_index == 0) {
-                    if (list_item != list.buffer[1] and list_item != list.buffer[2] and list_item != list.buffer[3]) {
-                        is_different_count += 1;
+                var char_is_different_count: u8 = 0;
+
+                for (list.buffer, 0..) |second_list_item, second_item_index| {
+                    if (item_index == second_item_index) {
+                        continue;
+                    }
+                    if (list_item != second_list_item) {
+                        char_is_different_count += 1;
                     }
                 }
-                if (item_index == 1) {
-                    if (list_item != list.buffer[0] and list_item != list.buffer[2] and list_item != list.buffer[3]) {
-                        is_different_count += 1;
-                    }
-                }
-                if (item_index == 2) {
-                    if (list_item != list.buffer[0] and list_item != list.buffer[1] and list_item != list.buffer[3]) {
-                        is_different_count += 1;
-                    }
-                }
-                if (item_index == 3) {
-                    if (list_item != list.buffer[0] and list_item != list.buffer[1] and list_item != list.buffer[2]) {
-                        is_different_count += 1;
-                    }
+
+                if (char_is_different_count == min_char_count_for_marker - 1) {
+                    is_different_count += 1;
                 }
             }
-            if (is_different_count == 4) {
+            if (is_different_count == min_char_count_for_marker) {
                 break :for_loop char_index + 1;
             }
         }
