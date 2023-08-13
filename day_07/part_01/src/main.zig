6c04a0e0893d00b30b442e8f12a687683f541b86
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-08-13 14:42:50 -0300

    Starts the day 07 part01

--- day_07/part_01/src/main.zig
+++ day_07/part_01/src/main.zig
@@ -1,45 +1,17 @@
 const std = @import("std");
 const utils = @import("utils");
-const BoundedArray = std.BoundedArray;
+const ArrayList = std.ArrayList;
+const ArrayListAligned = std.ArrayListAligned;
 
-const min_char_count_for_marker: comptime_int = 4;
+const DirectoryNode = struct { name: []const u8, parent: DirectoryNode, child: ArrayListAligned(DirectoryNode), size: i32 };
 
 pub fn main() !void {
-    var list = try BoundedArray(u8, min_char_count_for_marker).init(min_char_count_for_marker);
+    var allocator = std.heap.page_allocator;
+    var list: ArrayListAligned([]const u8, null) = ArrayList([]const u8).init(allocator);
+    defer list.deinit();
 
-    const string: []const u8 = utils.getStringFromPath("src/input.txt") catch "";
-
-    const index = for_loop: for (string, 0..) |character, char_index| {
-        std.log.info("{any}", .{char_index});
-        if (list.len != min_char_count_for_marker) {
-            try list.append(character);
-        } else {
-            const replaced_array: []const u8 = &list.buffer[1..].* ++ [1]u8{character};
-            try list.replaceRange(0, min_char_count_for_marker, replaced_array);
-        }
-        std.log.info("{s}", .{list.buffer});
-        if (char_index > min_char_count_for_marker) {
-            var is_different_count: u8 = 0;
-            for (list.buffer, 0..) |list_item, item_index| {
-                var char_is_different_count: u8 = 0;
-
-                for (list.buffer, 0..) |second_list_item, second_item_index| {
-                    if (item_index == second_item_index) {
-                        continue;
-                    }
-                    if (list_item != second_list_item) {
-                        char_is_different_count += 1;
-                    }
-                }
-
-                if (char_is_different_count == min_char_count_for_marker - 1) {
-                    is_different_count += 1;
-                }
-            }
-            if (is_different_count == min_char_count_for_marker) {
-                break :for_loop char_index + 1;
-            }
-        }
-    } else 0;
-    std.log.info("index: {any}", .{index});
+    try utils.getArrayListFromPath(allocator, &list, "src/input_demo.txt");
+    for (list.items) |item| {
+        std.log.info("list: {s}", .{item});
+    }
 }
